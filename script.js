e8d29e5f28349b74b10d66815b991b37a9c7fae6
mrnazmul68 <mrnazmul68@gmail.com>
2026-02-01 13:51:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("input");
+
+function buttonInput(input) {
+
+    display.value = display.value + input
+    
+}
+function clearDisplay() {
+    display.value = ''
+}
+
+function calculation() {
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Fuck you☹️😅"
+    }
+}
